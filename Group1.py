8a9caf59518335ec1cf1842f5ec0883bccbc8e00
samchenghowing <56993697+samchenghowing@users.noreply.github.com>
2022-10-15 22:28:39 +0800

    Added TO-DO

--- Group1.py
+++ Group1.py
@@ -3,7 +3,7 @@
 #pip install sshtunnel
 
 from datetime import date
-import sys, logging
+import logging
 import cx_Oracle
 from sshtunnel import SSHTunnelForwarder
 
@@ -31,12 +31,44 @@ def run(username="21089537d", password=""):
     initLogger()
     initSSHTunnel(username, password)
     logging.getLogger('SSHClient').info("Currnent user is " + ssh_user)
-    runSQLfile("initData.sql")
+    isAdminUser, userStatus = checkCurrentUser(username)
+    if isAdminUser:
+        runSQLfile("initData.sql")
+        #3 Records of books checked out as well as placed on hold (i.e. “reserved” by a patron to make sure the book is there when he/she gets to the library to check it out).
+    else:
+        if userStatus == False:
+            print("Your account is deactivated since you have expired books: ")
+            #2 The ability to deactivate a patron’s account if he/she does not return books after a specific period of time passes
+        else:
+            print("Welcome: ")
+            #4 Notifications when the desired book becomes available and reminders that a book should be returned to the library. Both could be sent by email and/or when patron logs in to the LMS.
 
-    # data = run_query("select * from emp")
-    # for row in data:
-    #     print (row[0], '-', row[1]) # this only shows the first two columns. To add an additional column you'll need to add , '-', row[2], etc.    
 
+def searchByTitle(Title):
+    #TO-DO please change this function
+    data = run_query("select * from book where Title=" + Title)
+    for row in data:
+        # print ("ISBN: ",row[0], ', -', row[1])
+        print (row)
+
+def searchByAuthor(Author):
+    #TO-DO please change this function
+    data = run_query("select * from book where Author Name=" + Author)
+    for row in data:
+        print (row[0], '-', row[1]) # this only shows the first two columns. To add an additional column you'll need to add , '-', row[2], etc.    
+
+def searchByCategory(bookName):
+    #TO-DO please change this function
+    data = run_query("select * from book where bookname=" + bookName)
+    for row in data:
+        print (row[0], '-', row[1]) # this only shows the first two columns. To add an additional column you'll need to add , '-', row[2], etc.    
+
+def checkCurrentUser(userName):
+    #TO-DO please change this function
+    # Deactivate a patron’s account if he/she does not return books after a specific period of time passes.
+    isAdmin = True
+    userStatus = False
+    return isAdmin, userStatus
 
 def initLogger():
     """Create a logger and log file named with today's day + connectionLog.txt """ 
@@ -55,6 +87,62 @@ def initSSHTunnel(username, password):
     ssh_pw = password
     cx_Oracle.init_oracle_client(lib_dir=local_lib_dir)
         
+def initDatabase():
+    """Create dummy data for testing and demo use"""
+    books = [
+        ('9781784975692', '001', 'The paper menagerie', 'Ken Liu', 'short-story', 89.9),
+    ]
+    publisher = [
+        ('001', 'Head of zeus'),
+    ]
+    librarian = [
+        ('Jane', date(2005, 2, 12)),
+    ]
+    reader = [
+        ('Joe', date(2006, 5, 23)),
+        ('John', date(2010, 10, 3)),
+    ]
+    loan_record = [
+        ('Joe', date(2006, 5, 23)),
+    ]
+    reserve_record = [
+        ('John', date(2010, 10, 3)),
+    ]
+    record_system = [
+        ('Joe', date(2006, 5, 23)),
+        ('John', date(2010, 10, 3)),
+    ]
+
+    try:
+        with cx_Oracle.connect(db_user, db_password, dsn) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""begin
+                                    execute immediate 'drop table books';
+                                    exception when others then
+                                    if sqlcode != -942 then
+                                        raise;
+                                    end if;
+                                end;""")
+                cursor.execute("""create table books (ISBN char(15) not null, PUBLISHER_ID char(3) not null, Title char(30), Author char(30), Category char(30), Price number(10))""")
+                #TO_DO modify this function:
+                # cursor.executemany("insert into BOOK(ISBN, data) values (:1, :2)", books, batcherrors = True)
+                # cursor.executemany("insert into PUBLISHER(PUBLISHER_ID, data) values (:1, :2)", publisher, batcherrors = True)
+                # cursor.executemany("insert into LIBRARIAN(LIBRARIAN_ID, data) values (:1, :2)", librarian, batcherrors = True)
+                # cursor.executemany("insert into READER(READER_ID, data) values (:1, :2)", reader, batcherrors = True)
+                # cursor.executemany("insert into LOAN_RECORD(LOAN_RECORD_ID, data) values (:1, :2)", loan_record, batcherrors = True)
+                # cursor.executemany("insert into RESERVE_RECORD(RESERVE_RECORD_ID, data) values (:1, :2)", reserve_record, batcherrors = True)
+                # cursor.executemany("insert into RECORD_SYSTEM(RECORD_SYSTEM, data) values (:1, :2)", record_system, batcherrors = True)
+                connection.commit()        
+                print("Database initize success")      
+                for error in cursor.getbatcherrors():
+                    print("Error", error.message.rstrip(), "at row offset", error.offset)
+
+    except cx_Oracle.Error as err:
+        print(str(err))
+        logging.getLogger('SSHClient').info("Error query: " + str(err))
+        # sys.exit(1)
+
+
 def tunnel():
   return SSHTunnelForwarder(
     ssh_host,
@@ -83,7 +171,6 @@ def run_query(query):
 
 def run_queryList(querys):
   with tunnel() as _:
-    logging.getLogger('SSHClient').info("run_queryList, querys are: " + str(querys))
     try:
         with cx_Oracle.connect(db_user, db_password, dsn) as connection:
             with connection.cursor() as cursor:
@@ -91,8 +178,7 @@ def run_queryList(querys):
                     currQuery = query.strip('\n')
                     cursor.execute(currQuery)
                     data = cursor.fetchall()
-                    print(data)
-                    logging.getLogger('SSHClient').info(currQuery + " result is:" + str(data))
+                    logging.getLogger('SSHClient').info(currQuery + " execute result is:" + str(data))
                 connection.commit()
                 
     except cx_Oracle.Error as err:
@@ -104,8 +190,11 @@ def runSQLfile(fileName):
     f = open(fileName)
     full_sql = f.read()
     sql_commands = full_sql.split(';')
-    run_queryList(sql_commands)
-    
+    if len(sql_commands) == 1:
+        run_query(sql_commands[0])
+    else:
+        run_queryList(sql_commands)
+
 if __name__ == '__main__':
     """Main function of the program, if running in terimal, it will take first argument as user name, 
         second argument as password for connecting to the department server via ssh"""
